43805da20e106cfbc80bd31760d18a7115df351f
cyril grinko <k.a.grinko@gmail.com>
2025-06-11 16:00:33 +0300

    format check script

---
+++ scripts/check_format_ci.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Launching clang-format check..."
+
+find src tests -name '*.hpp' -o -name '*.cpp' | while read file; do
+  clang-format -i "$file";
+done
+
+git diff --ignore-submodules --color > diff.txt
+
+cat diff.txt
+if [[ -s diff.txt ]]; then
+  echo "❌ clang-format failed";
+  exit 1;
+fi
+
+echo "✅ clang-format check passed"
+
+exit 0
