73e9f8677e1233725814348946f203857fa80430
cyril grinko <k.a.grinko@gmail.com>
2025-06-14 21:06:54 +0300

    testing script updated

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -13,9 +13,17 @@ if [[ "$GTEST_OUTPUT_XML" == 1 ]]; then
 fi
 
 if [[ "$GTEST_OUTPUT_XML" == 1 ]]; then
-  ./bin/lock_free_stack_unit --gtest_output=xml:tests/reports/unit_results.xml
+  ./bin/intrusive_list_unit --gtest_output=xml:tests/reports/intrusive_list_unit_results.xml
+  ./bin/lock_free_queue_unit --gtest_output=xml:tests/reports/lock_free_queue_unit_results.xml
+  # ./bin/lock_free_queue_stress --gtest_output=xml:tests/reports/lock_free_queue_stress_results.xml
+  ./bin/lock_free_stack_unit --gtest_output=xml:tests/reports/lock_free_stack_unit_results.xml
+  # ./bin/lock_free_stack_stress --gtest_output=xml:tests/reports/lock_free_stack_stress_results.xml
 else
+  ./bin/intrusive_list_unit
+  ./bin/lock_free_queue_unit
+  # ./bin/lock_free_queue_stress
   ./bin/lock_free_stack_unit
+  # ./bin/lock_free_stack_stress
 fi
 
 exit 0
