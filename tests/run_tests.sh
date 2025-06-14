21973569af01b9b984fc26f9b057df0d8d72ae0d
cyril grinko <k.a.grinko@gmail.com>
2025-06-14 18:41:41 +0300

    run_tests script update

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-if [ -n "$GTEST_OUTPUT_XML" ]; then
-	mkdir -p tests/reports
+# ./tests/run_tests.sh --output-xml
+
+GTEST_OUTPUT_XML=0
+
+if [[ "$1" == "--output-xml" ]]; then
+  GTEST_OUTPUT_XML=1
 fi
 
-# GTEST_OUTPUT_XML=1 ./tests/run_tests.sh
+if [[ "$GTEST_OUTPUT_XML" == 1 ]]; then
+  mkdir -p tests/reports
+fi
 
-if [ -n "$GTEST_OUTPUT_XML" ]; then
+if [[ "$GTEST_OUTPUT_XML" == 1 ]]; then
   ./bin/unit --gtest_output=xml:tests/reports/unit_results.xml
 else
   ./bin/unit
 fi
+
+exit 0
