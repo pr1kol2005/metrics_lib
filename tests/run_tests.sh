9b92750e19145111c047aa8016b13d8d9eef1cd3
cyril grinko <k.a.grinko@gmail.com>
2025-06-14 17:53:54 +0300

    tests prototype

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,8 +6,8 @@ fi
 
 # GTEST_OUTPUT_XML=1 ./tests/run_tests.sh
 
-# if [ -n "$GTEST_OUTPUT_XML" ]; then
-#     ./bin/another_test --gtest_output=xml:tests/reports/another_test_results.xml
-# else
-#     ./bin/another_test
-# fi
+if [ -n "$GTEST_OUTPUT_XML" ]; then
+  ./bin/unit --gtest_output=xml:tests/reports/unit_results.xml
+else
+  ./bin/unit
+fi
