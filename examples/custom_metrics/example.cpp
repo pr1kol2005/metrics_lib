fe03da6e5eeaa1b3aadd4bde04b93424e5101826
cyril grinko <k.a.grinko@gmail.com>
2025-06-15 03:32:12 +0300

    blank custom metric example

---
+++ examples/custom_metrics/example.cpp
@@ -0,0 +1,14 @@
+// TODO : Implement custom metric
+
+#include <metrics/registry.hpp>
+#include <metrics/writer.hpp>
+#include <metrics/ordered_metric_base.hpp>
+
+#include <random>
+#include <thread>
+
+using namespace metrics;
+
+int main () {
+  return 0;
+}
